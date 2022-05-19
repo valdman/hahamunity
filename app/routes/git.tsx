66e3eef822c17547498dfd09f041261df32c57eb
mghead <mghead@yandex.ru>
2022-05-19 13:35:11 +0000

    + nodegit

--- app/routes/git.tsx
+++ app/routes/git.tsx
@@ -1,4 +1,6 @@
-import Git, { Commit, Repository } from "nodegit";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Commit, Repository } from "nodegit";
 import { convertCompilerOptionsFromJson } from "typescript";
 
 function getMostRecentCommit(repository: Repository) {
@@ -9,10 +11,21 @@ function getCommitMessage(commit: Commit) {
 	return commit.message();
 }
 
-export async function getGitConnection() {
-	const repo = await Git.Repository.open("nodegit");
+export async function getGitLastMessage() {
+	const repo = await Repository.open("git@github.com:valdman/hahamunity.git");
 	const lastCommit = await getMostRecentCommit(repo);
 	const lastMessage = getCommitMessage(lastCommit);
 	console.log({lastMessage});
-	return repo;
+	return lastMessage;
+}
+
+export async function loader() {
+	const message = await getGitLastMessage();
+	return json({message});
+}
+
+export default function Git() {
+	const {message} = useLoaderData();
+	console.log({message})
+	return <>{message}</>;
 }
