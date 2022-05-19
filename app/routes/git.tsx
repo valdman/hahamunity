371772746f41bc01334f94405f62f80ecca84e4e
mghead <mghead@yandex.ru>
2022-05-19 13:37:18 +0000

    + git separate

--- app/routes/git.tsx
+++ app/routes/git.tsx
@@ -1,23 +1,7 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { Commit, Repository } from "nodegit";
-import { convertCompilerOptionsFromJson } from "typescript";
 
-function getMostRecentCommit(repository: Repository) {
-	return repository.getBranchCommit("master");
-}
-
-function getCommitMessage(commit: Commit) {
-	return commit.message();
-}
-
-export async function getGitLastMessage() {
-	const repo = await Repository.open("git@github.com:valdman/hahamunity.git");
-	const lastCommit = await getMostRecentCommit(repo);
-	const lastMessage = getCommitMessage(lastCommit);
-	console.log({lastMessage});
-	return lastMessage;
-}
+import { getGitLastMessage } from "~/git";
 
 export async function loader() {
 	const message = await getGitLastMessage();
